9cd46bb01930d4ae73717997fa5a9a724f0b80d9
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2020-05-19 12:05:37 +0300

    add footer & push it to bottom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,8 +19,8 @@
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet" integrity="{{ Sri::hash('css/app.css') }}" crossorigin="anonymous">
 </head>
-<body>
-    <div id="app"@stack('app.class')>
+<body class="flex flex-wrap h-full">
+    <div id="app" class="flex-shrink-0 w-full">
         <nav class="absolute w-full py-3">
             <div class="container mx-auto flex items-center">
                 <a class="block leading-tight text-black no-underline hover:text-black" href="{{ url('/') }}">
@@ -31,11 +31,11 @@
                 <div class="ml-auto">
                     <ul class="flex flex-row">
                         @guest
-                            <li><a class="px-3 py-2" href="{{ route('login') }}">{{ __('Sign In') }}</a></li>
-                            <li><a class="px-3 py-2 " href="{{ route('register') }}">{{ __('Sign Up') }}</a></li>
+                            <li class="mx-3"><a class="uppercase no-underline text-black hover:text-black px-3 py-2" href="{{ route('login') }}">{{ __('Sign In') }}</a></li>
+                            <li class="mx-3"><a class="bg-shrtn text-white uppercase no-underline font-bold hover:text-white rounded-lg px-6 py-3 " href="{{ route('register') }}">{{ __('Sign Up') }}</a></li>
                         @else
-                            <li><a href="{{ route('login') }}">{{ __('Settings') }}</a></li>
-                            <li>
+                            <li class="mx-3"><a href="{{ route('login') }}">{{ __('Settings') }}</a></li>
+                            <li class="mx-3">
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                     document.getElementById('logout-form').submit();"
@@ -54,30 +54,30 @@
 
         @yield('content')
 
-        <footer>
-            <div class="container mx-auto py-6">
-                <div class="flex flex-wrap items-center">
-                    <div class="w-1/3">
-                        <span class="uppercase tracking-logo text-2xl">{{ config('app.name', 'SHRTN') }}</span><br>
-                        <span class="uppercase text-sm">Make long links short</span>
-                    </div>
-                    <div class="w-1/3">
-                        <p>
-                            <b>Total links shortened:</b>
-                            <br>1,234,567
-                        </p>
-                    </div>
-                    <div class="w-1/3">
-                        <ul class="flex flex-wrap">
-                            <li class="w-1/2"><a href="#">Shorten</a></li>
-                            <li class="w-1/2"><a href="#">GitHub</a></li>
-                            <li class="w-1/2"><a href="#">Statistics</a></li>
-                            <li class="w-1/2"><a href="#">Browser extensions</a></li>
-                        </ul>
-                    </div>
+    </div>
+    <footer class="w-full mt-auto">
+        <div class="container mx-auto py-6">
+            <div class="flex flex-wrap items-center">
+                <div class="w-1/3">
+                    <span class="uppercase tracking-logo text-2xl">{{ config('app.name', 'SHRTN') }}</span><br>
+                    <span class="uppercase text-sm">Make long links short</span>
+                </div>
+                <div class="w-1/3">
+                    <p>
+                        <b>Total links shortened:</b>
+                        <br>1,234,567
+                    </p>
+                </div>
+                <div class="w-1/3">
+                    <ul class="flex flex-wrap">
+                        <li class="w-1/2"><a href="#">Shorten</a></li>
+                        <li class="w-1/2"><a href="#">GitHub</a></li>
+                        <li class="w-1/2"><a href="#">Statistics</a></li>
+                        <li class="w-1/2"><a href="#">Browser extensions</a></li>
+                    </ul>
                 </div>
             </div>
-        </footer>
-    </div>
+        </div>
+    </footer>
 </body>
 </html>
