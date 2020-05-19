589ea3da92540df1ca4fd99544d0545bfd70ee88
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2020-05-19 12:05:47 +0300

    fix container

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="pt-20 container mx-auto">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h3><b>Statistics for</b>: {{ $url->url }}</h3>
