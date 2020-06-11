3daa810da71bfaffaed5840a8a6aef71311fc462
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2020-06-11 20:10:07 +0300

    add an empty state

--- resources/js/components/LinkCard.vue
+++ resources/js/components/LinkCard.vue
@@ -1,13 +1,18 @@
 <template>
-    <div class="linkCard">
-        <div class="linkCard__heading">
-            <h2 v-text="shortLink"></h2>
-            <p class="text-gray-500" v-text="longLink"></p>
+    <div>
+        <div v-if="shortLink.length == 0">
+            Start by shortening a long link
         </div>
-        <a class="linkCard__copy" href="#" :id="'copy-' + id" :data-clipboard-text="shortLink">
-            <span class="sr-only">Copy short link</span>
-            <svg class="not-sr-only" width="26" height="23" xmlns="http://www.w3.org/2000/svg"><g transform="translate(1 1)" stroke="#000" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><rect x="8.4" y="5.95" width="15.6" height="14.95" rx="2"/><path d="M3.6 14.95H2.4c-1.325 0-2.4-1.03-2.4-2.3V2.3C0 1.03 1.075 0 2.4 0h10.8c1.325 0 2.4 1.03 2.4 2.3v1.15"/></g></svg>
-        </a>
+        <div class="linkCard" v-else>
+            <div class="linkCard__heading">
+                <h2 v-text="shortLink"></h2>
+                <p class="text-gray-500" v-text="longLink"></p>
+            </div>
+            <a class="linkCard__copy" href="#" :id="'copy-' + id" :data-clipboard-text="shortLink">
+                <span class="sr-only">Copy short link</span>
+                <svg class="not-sr-only" width="26" height="23" xmlns="http://www.w3.org/2000/svg"><g transform="translate(1 1)" stroke="#000" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><rect x="8.4" y="5.95" width="15.6" height="14.95" rx="2"/><path d="M3.6 14.95H2.4c-1.325 0-2.4-1.03-2.4-2.3V2.3C0 1.03 1.075 0 2.4 0h10.8c1.325 0 2.4 1.03 2.4 2.3v1.15"/></g></svg>
+            </a>
+        </div>
     </div>
 </template>
 
@@ -47,16 +52,21 @@
 <style lang="scss" scoped>
 .linkCard {
     display: flex;
+    flex-grow: 1;
     border-radius: 9px;
     @apply shadow-md bg-white;
 }
+
 .linkCard__heading {
+    flex-grow: 1;
     padding: .75rem 1rem;
 }
+
 h2 {
     font-weight: bold;
     @apply text-xl;
 }
+
 .linkCard__copy {
     display: flex;
     margin-left: auto;
