2470ba11f7acbd9f5903771b557f5837f89e5768
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2020-09-08 18:11:57 +0300

    pass UrlController class directly

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UrlController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('/shorten', 'ShortenerController');
+Route::apiResource('/shorten', UrlController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
